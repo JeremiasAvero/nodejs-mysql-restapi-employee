10f8f757c89c5e0de5b1fb01ce33655af2a11a18
JeremiasAvero <jeremiasavero98@gmail.com>
2023-12-13 12:12:50 -0300

    index message changed

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,5 +1,5 @@
 import {pool} from '../db.js'
-export const index = (req, res) => res.json({ message: "welcome to my api" });
+export const index = (req, res) => res.json({ message: "Welcome to my Employees api" });
 export const ping = async (req,res) => {
     const [result] = await pool.query('SELECT "pong" as result');
     res.json(result[0]);
